4a25a06b90a632c97c6b25f4ccc52a502453668f
kingarthurdavis78 <kingarthurdavis78@gmail.com>
2023-05-10 22:45:31 -0600

    almost finish evil hangman

--- EvilHangman/src/hangman/EvilHangman.java
+++ EvilHangman/src/hangman/EvilHangman.java
@@ -1,8 +1,56 @@
 package hangman;
 
-public class EvilHangman {
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Scanner;
 
+public class EvilHangman {
     public static void main(String[] args) {
+
+        File dictionary = new File(args[0]);
+        int word_length = Integer.parseInt(args[1]);
+        int num_guesses = Integer.parseInt(args[2]);
+        EvilHangmanGame game = new EvilHangmanGame();
+        try {
+            game.startGame(dictionary, word_length);
+        }
+        catch (IOException | EmptyDictionaryException ex) {
+            ex.printStackTrace();
+        }
+        Scanner in = new Scanner(System.in);
+
+        for (int i=num_guesses;i>0;i--) {
+            System.out.printf("You have %d guesses left%n", i);
+            System.out.printf("Used Letters: %s%n", game.guessed_letters);
+            System.out.printf("Word: %s%n", game.word);
+            System.out.print("Enter guess: ");
+            String s = in.nextLine();
+            char guess = s.charAt(0);
+            try {
+                game.makeGuess(guess);
+            }
+            catch (GuessAlreadyMadeException e) {
+                throw new RuntimeException(e);
+            }
+            if (!game.word.contains(s)) {
+                System.out.printf("Sorry there are no %s's%n", s);
+            }
+            else {
+                int result = 0;
+                for (int j = 0; j < game.word.length(); j++) {
+                    char letter = game.word.charAt(j);
+                    if (String.valueOf(letter).equals(s)) {
+                        result++;
+                    }
+                }
+                System.out.printf("Yes, there is %d %s%n", result, s);
+            }
+            System.out.println();
+
+        }
+        System.out.println("You lose!");
+        System.out.println(game.word_set.first());
 
     }
 
--- EvilHangman/src/hangman/EvilHangmanGame.java
+++ EvilHangman/src/hangman/EvilHangmanGame.java
@@ -2,23 +2,117 @@ package hangman;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.*;
 
 public class EvilHangmanGame implements IEvilHangmanGame {
+    SortedSet<String> word_set = new TreeSet<>();
+    SortedSet<Character> guessed_letters = new TreeSet<>();
+
+    Map<String, SortedSet<String>> word_map = new HashMap<>();
+    String word;
 
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
+        word_set.clear();
+        Scanner scanner = new Scanner(dictionary);
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+            if (word.length() == wordLength) {
+                word_set.add(word);
+            }
+        }
+        word = "-".repeat(wordLength);
+        if (word_set.isEmpty()) {
+            throw new EmptyDictionaryException();
+        }
+    }
 
+    String getKey(String word, char guess) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == guess) {
+                result.append(guess);
+            }
+            else {
+                result.append("_");
+            }
+        }
+        return result.toString();
+    }
+
+    int num_letters(String word, char guess) {
+        int result = 0;
+        for (int i=0; i<word.length(); i++) {
+            char letter = word.charAt(i);
+            if (guess == letter) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    String right_most(String word1, String word2, char guess) {
+        for (int i=0; i<word1.length();i++) {
+            char c1 = word1.charAt(i);
+            char c2 = word2.charAt(i);
+            if (c1 == guess && c2 == guess) {
+                continue;
+            }
+            if (c1 != guess && c2 == guess) {
+                return word1;
+            } else if (c1 == guess) {
+                return word2;
+            }
+        }
+        return word1;
     }
 
     @Override
-    public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
-        return null;
+    public Set<String> makeGuess(char guess1) throws GuessAlreadyMadeException {
+        Character guess = Character.toLowerCase(guess1);
+        if (guessed_letters.contains(guess)) {
+            throw new GuessAlreadyMadeException();
+        }
+        guessed_letters.add(guess);
+
+        word_map.clear();
+        for (String word:word_set) {
+            String key = getKey(word, guess);
+            if (!word_map.containsKey(key)) {
+                word_map.put(key, new TreeSet<>());
+            }
+            word_map.get(key).add(word);
+        }
+
+        String most = null;
+        for (String k:word_map.keySet()) {
+            if (most == null) {
+                most = k;
+                continue;
+            }
+            if (word_map.get(k).size() > word_map.get(most).size()) {
+                most = k;
+            } else if (word_map.get(k).size() == word_map.get(most).size()) {
+                if (most.contains(guess.toString()) && k.contains(guess.toString())) {
+                    if (num_letters(k, guess) < num_letters(most, guess)) {
+                        most = k;
+                    }
+                    if (num_letters(k, guess) == num_letters(most, guess)) {
+                        most = right_most(most, k, guess);
+                    }
+                } else if (!k.contains(guess.toString())) {
+                    most = k;
+                }
+
+            }
+        }
+        word = most;
+        word_set = word_map.get(most);
+        return word_set;
     }
 
     @Override
     public SortedSet<Character> getGuessedLetters() {
-        return null;
+        return guessed_letters;
     }
 }
